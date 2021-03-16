fe3d443dc6082f73dafb4c212bc87c683ca9ff02
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2021-03-16 20:44:43 +0900

    Add h3 comment

--- server/hello.js
+++ server/hello.js
@@ -1,8 +1,11 @@
 import express from 'express'
 const app = express()
 
+// It failed when deploy.
+// https://github.com/unjs/h3/issues/21
 // import { createApp } from 'h3'
 // const app = createApp()
+// app.use('/', (_req, res) => {
 
 app.get('/', (_req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
